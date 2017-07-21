715e9ea1e5821fd486cb95f743472202104898c7
olaoluwa ogunwa <olaoluwaogunwa@olaoluwas-mbp.attlocal.net>
2017-07-21 18:38:27 -0500

    initial commit

--- script.py
+++ script.py
@@ -3,9 +3,9 @@
 import os
 import requests
 import json
+import datetime
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
-#from dateutil import relativedelta
 from dotenv import load_dotenv, find_dotenv
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
@@ -18,7 +18,7 @@ load_dotenv(find_dotenv())
 recipient_email =  os.environ.get('RECIPIENT_EMAIL')
 from_email = os.environ.get('SENDER_EMAIL')
 
-def anniversary(per_page, current_page, anniversaries=[]):
+def employee_anniversary(per_page, current_page, anniversaries=[]):
 	url = 'https://rimon.namely.com/api/v1/profiles.json?filter[status]=active&per_page=' + str(per_page) + '&page=' + str(current_page)
 
 	token = os.environ.get('TOKEN')
@@ -34,21 +34,22 @@ def anniversary(per_page, current_page, anniversaries=[]):
 	print "Total records: " + str(total_records) + "\n"
 	print "Current Page: " + str(current_page)
 	count = 1
+
 	for row in json_data['profiles']:
-		six_month_mark = ""
-		today = datetime.today().strftime('%Y-%m-%d')
-		employed_date_obj = datetime.strptime(str(row['start_date']),'%Y-%m-%d')
+		today = datetime.today()
 		if row['start_date']:
-			groups = row['links']['groups']
-			if groups != None:
-				for group in groups:
-					if group['name'].lower() == 'attorney':
-						six_month_mark = (employed_date_obj + relativedelta(months=6)).strftime('%Y-%m-%d') # datetime.strptime(row['start_date'], '%Y-%m-%d').replace(month = employed_date_obj.month+6)
-						if today == six_month_mark:
-							employee = { "fullname": str(row["first_name"]) + " " + str(row["last_name"]), "employed_date": employed_date_obj.strftime('%Y-%m-%d') }
-							anniversaries.append(employee)
-					break
-		print row["first_name"], employed_date_obj, "<===>", six_month_mark, "|||", today
+			employed_date_obj = datetime.strptime(row['start_date'], '%Y-%m-%d') 
+			year_difference = today.year - employed_date_obj.year
+
+			current_year_employment_date = employed_date_obj + relativedelta(years=year_difference)
+			days_left = (current_year_employment_date - today).days + 1
+
+			if days_left == 14:
+				employee = { "fullname": str(row["first_name"], ) + " " + str(row["last_name"],), "anniversary_date": current_year_employment_date.strftime('%Y-%m-%d'), "employed_date": employed_date_obj.strftime('%Y-%m-%d'), "number_of_years": year_difference }
+				anniversaries.append(employee)
+
+		else:
+			print ""
 			
 		count = count + 1
 	return (anniversaries, (total_records > (per_page*current_page)))
@@ -58,14 +59,14 @@ def anniversary(per_page, current_page, anniversaries=[]):
 per_page = 50
 current_page = 1
 
-api_call = anniversary(per_page, current_page, [])
+api_call = employee_anniversary(per_page, current_page, [])
 
 anniversaries = api_call[0]
 run_again = api_call[1]
 
 while run_again:
 	current_page += 1
-	api_call = anniversary(per_page, current_page, anniversaries)
+	api_call = employee_anniversary(per_page, current_page, anniversaries)
 	anniversaries = api_call[0]
 	run_again = api_call[1]
 
@@ -74,15 +75,15 @@ print anniversaries
 
 total_celebrants = len(anniversaries)
 if total_celebrants > 0:
-	msg_body = "<p>It is now the 6 month mark since <b>" + str(total_celebrants) + "</b> employee(s) were hired</p>"
+	msg_body = "<p>Hey there,</p><p><b>The following people have their Rimoniversaries coming up in just two weeks! Please send the Rimoniversary gift to arrive by the scheduled date. Thank you.</b></p>"
 	for celebrant_info in anniversaries:
-		msg_body += "<p><b>" + celebrant_info['fullname'] + "</b> - " + celebrant_info['employed_date'] + "</p>"
-
+		msg_body += "<p><b>" + celebrant_info['fullname'] + "</b>   -   Rimoniversary Date: " + celebrant_info['anniversary_date'] + " </p>"
+		# msg_body += "<p><b>" + celebrant_info['fullname'] + "</b> - Rimoniversary Date: " + celebrant_info['anniversary_date'] + " <b>" + str(celebrant_info['number_of_years']) + "</b> year(s) in service</p>"
 	msg = MIMEMultipart()
 	msg['From'] = from_email
 	msg['To'] = recipient_email
 	import os
-	msg['Subject'] = "6 months hire Reminders - " + datetime.today().strftime('%Y-%m-%d')
+	msg['Subject'] = "Anniversary Reminder - " + datetime.today().strftime('%Y-%m-%d')
 	msg.attach(MIMEText(msg_body, 'html'))
 
 	text = msg.as_string()
